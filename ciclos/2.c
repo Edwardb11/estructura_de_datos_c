a8874f9e8f7d63a9102acb214cab9a54101de687
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2021-07-08 10:24:14 -0400

    Update 2.c

--- ciclos/2.c
+++ ciclos/2.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 struct clientes{
-char nombre[50];
-char apellido[50]; int telefono;
-char direccion[50]; int cedula;
-char estado[50]; }; //mis estructuras
+  char nombre[50];
+  char apellido[50]; int telefono;
+  char direccion[50]; int cedula;
+  char estado[50]; }; //mis estructuras
 int main(){
-int opcion;
-printf("ELiga una opcion:");
-printf("1\n Mostrar 2\nSalir "); scanf("%d",&opcion);
-if(opcion==1){
-struct clientes clientes1={"Edward","Brito",8498564014, "La Vega",93938261819,"Soltero"};
-puts(clientes1.nombre);
-puts(clientes1.apellido);
-printf("\Telefono: %d\n",clientes1.telefono); puts(clientes1.direccion);
-printf("\Cedula: %d\n",clientes1.cedula); puts(clientes1.estado);
-}
+  int opcion;
+  printf("ELiga una opcion:");
+  printf("1\n Mostrar 2\nSalir "); scanf("%d",&opcion);
+  if(opcion==1){
+   struct clientes clientes1={"Edward","Brito",8498564014, "La Vega",93938261819,"Soltero"};
+    puts(clientes1.nombre);
+    puts(clientes1.apellido);
+    printf("\Telefono: %d\n",clientes1.telefono); puts(clientes1.direccion);
+    printf("\Cedula: %d\n",clientes1.cedula); puts(clientes1.estado);
+  }
 else{
-printf("Salir"); }
+  printf("Salir"); }
 }
